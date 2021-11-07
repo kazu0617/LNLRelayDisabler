8303c52b256586a04ca9fe1383d8f136f29ed901
kazu0617 <kazu0617@protonmail.com>
2021-11-07 18:58:14 +0900

    re-construct for NeosLNLRelayDisabler

---
+++ NeosLNLRelayDisabler/NeosLNLRelayDisabler.cs
@@ -0,0 +1,42 @@
+﻿using FrooxEngine;
+using HarmonyLib;
+using NeosModLoader;
+using System.Collections.Generic;
+
+namespace NeosLNLRelayDisabler
+{
+    public class NeosLNLRelayDisabler : NeosMod
+    {
+        public override string Name => "NeosLNLRelayDisabler";
+        public override string Author => "kazu0617";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/kazu0617/NeosLNLRelayDisabler/";
+        public override void OnEngineInit()
+        {
+            Harmony harmony = new Harmony("net.kazu0617.NeosLNLRelayDisabler");
+
+            harmony.PatchAll();
+            Msg("Hooks installed successfully!");
+        }
+
+//        [HarmonyPatch(typeof(NetX.LNL_Manager), "GetSupportedSchemes")]
+//        class NeosLNLRelayDisablerPatch
+//        {
+//            static bool Prefix(ref List<string> schemes)
+//            {
+//                schemes.Add("lnl");
+//                return false; //try to skip original one
+//            }
+//        }
+
+        [HarmonyPatch(typeof(NetX.LNL_Connection), "ConnectToRelay")]
+        class NeosLNLRelayDisablerPatch2
+        {
+            static bool Prefix()
+            {
+                return false; //try to skip original one
+            }
+        }
+
+    }
+}
--- NeosLNLRelayDisabler/Properties/AssemblyInfo.cs
+++ NeosLNLRelayDisabler/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ModNameGoesHere")]
+[assembly: AssemblyTitle("LiteDBPassChcecker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ModNameGoesHere")]
+[assembly: AssemblyProduct("LiteDBPassChcecker")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -33,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("ja-JP")]
